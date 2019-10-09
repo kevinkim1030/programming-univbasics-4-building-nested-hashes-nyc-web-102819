af455ae7b092d850a994a1101594fd9657cd8deb
Kevin Kim <kevin.kim1030@gmail.com>
2019-10-09 23:56:37 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -37,7 +37,7 @@ def monopoly_with_fourth_tier
     pieces: 4,
     names: {
       reading_railroad: {
-        "mortage value" => "$100"
+        "mortage_value" => "$100"
       },
       pennsylvania_railroad: {},
       b_and_o_railroad: {},
