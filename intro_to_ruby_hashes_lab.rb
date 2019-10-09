044fa3bf15a229e5cac7d50641ce64b57a741a71
Kevin Kim <kevin.kim1030@gmail.com>
2019-10-09 23:45:19 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,13 +20,13 @@ def monopoly_with_third_tier
       key6: " ",
       key7: " ",
       key8: " "
-    }
+    },
     rent_in_dollars: {
       key1: " ",
       key2: " ",
       key3: " ",
       key4: " "
     }
-  
+  }
   }
 end
