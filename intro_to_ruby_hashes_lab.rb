56879bff919e15da02e3cbc9850b8ea0afa5cf00
Kevin Kim <kevin.kim1030@gmail.com>
2019-10-09 23:37:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,14 +8,15 @@ end
 
 def monopoly_with_second_tier 
 	{railroads: {
-	  pieces: 4,
-	 rent_in_dollars: "yea"
+	  pieces: 4
 	  }
 	}
 end
 
 def monopoly_with_third_tier
-
+  {railroads: {
+    rent_in_dollars: "truthy"
+  }
 end
 
 def monopoly_with_fourth_tier
